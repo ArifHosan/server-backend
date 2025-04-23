e9f805e1f8f0be4d610f694b59cef7e25550a970
Arif <hosan.arif0@gmail.com>
2025-04-23 16:28:31 +0200

    added retry and error checks

--- src/ha/ha.controller.ts
+++ src/ha/ha.controller.ts
@@ -1,7 +1,10 @@
 import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
 import { Controller, Get, Inject, Param } from '@nestjs/common';
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 import { Agent } from 'https';
+
+axiosRetry(axios, { retries: 3 });
 
 @Controller('ha')
 export class HaController {
@@ -19,15 +22,21 @@ export class HaController {
         return JSON.parse(cachedData) as Record<string, any>;
       } else {
         const httpsAgent = new Agent({ rejectUnauthorized: false });
-        const response = await axios.get(ASEAG_ENDPOINT, { httpsAgent });
-        const data = response.data as Record<string, any>;
+        return axios
+          .get(ASEAG_ENDPOINT, { httpsAgent })
+          .then(async (response) => {
+            const data = response.data as Record<string, any>;
 
-        await this.cacheManager.set(
-          cacheKey,
-          JSON.stringify(data),
-          1000 * 60 * 10,
-        );
-        return data;
+            await this.cacheManager.set(
+              cacheKey,
+              JSON.stringify(data),
+              1000 * 60 * 10,
+            );
+            return data;
+          })
+          .catch((error) => {
+            console.error('Error fetching data from ASEAG endpoint:', error);
+          });
       }
     });
   }
